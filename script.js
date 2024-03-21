a4c053d7c02b234ecbc2199c2fda3aaf50af877c
JohnnyUtah666 <porksloppp@gmail.com>
2024-03-21 14:17:05 -0700

    rewrite function to target proper variables

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let pass1 = document.querySelector('#password');
 let pass2 = document.querySelector('#confirmPassword');
+let result = document.querySelector('.match');
 
 function checkPassword () {
-    match.innerText = pass1.value == pass2.value ? 'Matching' : "Noth Matching";
+    result.innerText = pass1.value == pass2.value ? 'Passwords Match \u2705' : "Passwords Do Not Match \u274c";
 }
 
 pass1.addEventListener('keyup', () => {
